8d40ede5f67e1c2e12c4853a5ef8e73f87d1bde1
Vincent <vincent.debuisson@gmail.com>
2022-01-06 15:34:41 +0100

    ajout d'une modif

--- src/main/java/fr/semifir/testDemo/employes/EmployeController.java
+++ src/main/java/fr/semifir/testDemo/employes/EmployeController.java
@@ -25,6 +25,7 @@ public class EmployeController {
 
     @GetMapping("{id}")
     public ResponseEntity<EmployeDTO> findById(@PathVariable Long id) {
+        System.out.println("salut à tous");
         try {
             Optional<EmployeDTO> employeDTO = this.service.findById(id);
             return ResponseEntity.ok(employeDTO.get());
